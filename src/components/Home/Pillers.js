155838239636afb1e586a9701ef2923a729c39e7
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2024-02-07 09:24:12 +0100

    Update Pillers.js
    Commit

--- src/components/Home/Pillers.js
+++ src/components/Home/Pillers.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Pillars1 from '../../assets/images/messi.png'
 import Pillars2 from '../../assets/images/babywen.png'
 import Pillars3 from '../../assets/images/hen.png'
-import Pillars3 from '../../assets/images/dexterhero.svg'
-import Pillars3 from '../../assets/images/baby jupiter 2.svg'
-import Pillars3 from '../../assets/images/hen.png'
+import Pillars4 from '../../assets/images/dexterhero.svg'
+import Pillars5 from '../../assets/images/baby jupiter 2.svg'
+import Pillars6 from '../../assets/images/hen.png'
 
 
 
@@ -76,7 +76,7 @@ export default function Pillers() {
         <div className=' col-span-12 lg:col-span-6 xl:col-span-4 mt-4 sm:mt-0 ' >
           <div className='  relative text-center pillerhvr ' >
             <div class="image-container">
-              <img src={Pillars1} className=' rounded-[32px] object-cover w-full lg:w-[435px] h-[435px] mx-auto ' />
+              <img src={Pillars4} className=' rounded-[32px] object-cover w-full lg:w-[435px] h-[435px] mx-auto ' />
               <div class="image-overlay"></div>
               <div class="image-text">
                 <p className='f-f-m text-tiny text-white ' >Success of PuppyLove will depend on its community. As reached in consensus, PuppyLove will stand on united strength of its community as well as that of human population. The puppies have left Koja to join human population, humans can't afford to fail them. United we stand to make PuppyLove great like their parents - Doge, ShibInu, Floki and few others.</p>
@@ -94,7 +94,7 @@ export default function Pillers() {
         <div className=' col-span-12 lg:col-span-6 xl:col-span-4  mt-4 sm:mt-0 ' >
           <div className='  relative text-center' >
             <div class="image-container">
-              <img src={Pillars2} className=' rounded-[32px] object-cover w-full lg:w-[435px] h-[435px] mx-auto ' />
+              <img src={Pillars5} className=' rounded-[32px] object-cover w-full lg:w-[435px] h-[435px] mx-auto ' />
               <div class="image-overlay"></div>
               <div class="image-text-2">
                 <p className='f-f-m text-tiny text-white ' >Based on the consensus reached, PuppyLove will stand firmly on Innovative developers amongst humans to build cutting-edge blockchain utilities that would empower human population. PuppyLove uses this medium to invite Innovative developers within the community to come forward and volunteer in building blockchain utilities that would empower both the community and human population at large.</p>
@@ -111,7 +111,7 @@ export default function Pillers() {
         <div className=' col-span-12 lg:col-span-12 xl:col-span-4  mt-4 sm:mt-0 ' >
           <div className='  relative text-center' >
             <div class="image-container">
-              <img src={Pillars3} className=' rounded-[32px] object-cover w-full lg:w-[435px] h-[435px] mx-auto ' />
+              <img src={Pillars6} className=' rounded-[32px] object-cover w-full lg:w-[435px] h-[435px] mx-auto ' />
               <div class="image-overlay"></div>
               <div class="image-text">
                 <p className='f-f-m text-tiny text-white ' >PuppyLove will support puppies and human kids. A good percentage of the project is dedicated towards this - Support for puppies shelters and children causes. The community will be given opportunity from time to time to vote on charity this Support should go to. The support will go to charities that truly need the support anywhere in the world.</p>
